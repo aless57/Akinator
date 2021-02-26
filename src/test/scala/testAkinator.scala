418cf0b89bc06536afb4942d2b40c9e40ff811a7
MatthieuGalante <Matthieu.galante5@etu.univ-lorraine.fr>
2021-02-26 17:54:27 +0100

    ajout jeuLog et jeuApprentissage

--- src/test/scala/testAkinator.scala
+++ src/test/scala/testAkinator.scala
@@ -6,6 +6,8 @@ import org.scalatest._
 import scala.io.Source
 import objAkinator._
 
+import scala.::
+
 class testAkinator extends FunSuite{
 
   val a = Question("Est-ce qu'il a des ailes ?",
@@ -26,4 +28,30 @@ class testAkinator extends FunSuite{
     val ret = jeuSimple(a,List("n","n","o").iterator)
     assert(ret)
   }
+
+  test("jeuLog 1"){
+    val ret = jeuLog(a,List("o","o","o","o").iterator)
+    assert(ret.equals(List("o","o","o","o")))
+  }
+
+  test("jeuLog 2"){
+    val ret = jeuLog(a,List("n","n","o").iterator)
+    assert(ret.equals(List("n","n","o")))
+  }
+
+  //test qu'il me reste à faire
+
+  /*test("jeuApprentissage 1"){
+    val ret = jeuApprentissage(a,List("o","o","o","n").iterator)
+    val a2 = Question("Est-ce qu'il a des ailes ?",
+      Question("Est-ce qu'il a des plumes ?",
+        Question("Est-ce qu'il a un goitre ?",
+          Animal("Pelican"),Animal("Pigeon")),
+        Question("Est-ce qu'il a des poils ?",
+          Animal("Chauve-souris"),Animal("Pterodactyle"))),
+      Question("Est-ce qu'il ronronne ?",
+        Animal("Chat"),Animal("Chien")))
+    assert(ret.equals(a2))
+  }**/
+
 }
